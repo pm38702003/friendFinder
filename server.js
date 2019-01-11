6b4a4065824029d777a459516e33dc307baed5bc
pm38702003 <pm38702003@yahoo.com>
2019-01-10 21:38:23 -0500

    changes

--- server.js
+++ server.js
@@ -8,14 +8,15 @@ var data = require("./app/routing/apiRoutes.js");
 // Sets up the Express App
 // =============================================================
 var app = express();
-//var PORT = process.env.PORT || 3000;
-var PORT = process.env.PORT || port;
+var PORT = process.env.PORT || 3000;
+//var PORT = process.env.PORT || port;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 routes.app = app;
+
 
 
 data.app = app;   //api call get and add //stored for access in module
